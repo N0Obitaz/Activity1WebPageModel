1e4b6394c88dd8fdfd7c517102e4aaec175afdf3
N0obitaz <charlesbernard.balaguer.041504@gmail.com>
2025-09-06 11:47:44 +0800

    Add project files.

---
+++ Pages/PersonalDetails/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Activity1WebPageModel.Pages.PersonalDetails
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ Pages/ScholasticDetails/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Activity1WebPageModel.Pages.ScholasticDetails
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
